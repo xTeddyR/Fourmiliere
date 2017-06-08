7787826ed72c9495335fddd045167dc310465c80
Sorion <maxime.pelte@gmail.com>
2017-06-08 23:05:00 +0200

    Adding resources colors, updating binding color

---
+++ FourmilliereAL/ConvertisseurValeur/ConvertisseurAttitudeVersCouleurs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace FourmilliereAL
+{
+    public class ConvertisseurAttitudeVersCouleurs : ConvertisseurValeurBase<ConvertisseurAttitudeVersCouleurs>
+    {
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            App.Current.FindResource("PrimaryLightBrush");
+
+            if(value.ToString() == "AttitudeCombattante") {
+                return App.Current.FindResource("SecondaryLightBrush");
+            }
+
+            return App.Current.FindResource("PrimaryLightBrush");
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

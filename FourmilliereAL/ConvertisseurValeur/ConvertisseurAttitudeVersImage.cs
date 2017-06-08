a83f5c71b730ff10567ad4d974cdb41592767e85
Sorion <maxime.pelte@gmail.com>
2017-06-08 18:17:01 +0200

    Adding conveter to converter class to image path, adding default ant

---
+++ FourmilliereAL/ConvertisseurValeur/ConvertisseurAttitudeVersImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace FourmilliereAL
+{
+    public class ConvertisseurAttitudeVersImage : ConvertisseurValeurBase<ConvertisseurAttitudeVersImage>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value.ToString() == "AttitudeCombattante") {
+                return "../../Media/warrior-ant.png";
+            }
+
+            return "../../Media/default-ant.png";
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FourmilliereAL/ConvertisseurValeur/ConvertisseurValeurBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace FourmilliereAL
+{
+     public abstract class ConvertisseurValeurBase<T> : MarkupExtension, IValueConverter
+         where T : class, new()
+    {
+        #region Membres Privés
+
+        /// <summary>
+        /// Une instance statique de ce convertiseur de valeur
+        /// </summary>
+        private static T mConverter = null;
+
+        #endregion
+
+        #region Markup Extension Methods
+
+        /// <summary>
+        /// Retourne une instance statique de ce convertiseur de valeur
+        /// </summary>
+        /// <param name="serviceProvider">The service provider</param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return mConverter ?? (mConverter = new T());
+        }
+
+        #endregion
+
+        #region Value Converter Methods
+
+        /// <summary>
+        /// La meéthode pour convertir une valeur en une autre
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        /// <summary>
+        /// Méthode pour convertir une valeur à sa source d'origine
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+        #endregion
+
+    }
+}

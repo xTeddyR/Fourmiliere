96376bf9fe893ae954ce5e54becbb8c95744003a
Sorion <maxime.pelte@gmail.com>
2017-06-20 00:27:48 +0200

    Adding ViewModel & design model for ant

---
+++ FourmilliereAL.Core/Creatures/FourmiListItemViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace FourmilliereAL.Core { 
+
+    /// <summary>
+    /// un View Model pour chaque item de la liste de fourmis
+    /// </summary>
+    public class FourmiListItemViewModel
+    {
+        /// <summary>
+        /// Le nom à afficher d'un item
+        /// </summary>
+        public string Nom { get; set; }
+
+        /// <summary>
+        /// La vie d'un item
+        /// </summary>
+        public int Vie { get; set; }
+
+        /// <summary>
+        /// La liste des étapes d'un item
+        /// </summary>
+        public List<Etape> ListEtape { get; set; }
+
+        /// <summary>
+        /// Le comportement d'un item
+        /// </summary>
+        public Attitude Comportement { get; set; }
+    }
+}

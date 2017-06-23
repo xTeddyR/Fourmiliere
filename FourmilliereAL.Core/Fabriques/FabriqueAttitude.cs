43c78572a4d24975e2409bf98f38a5134d984702
Jérémy BOEHM <jeremyboehm77@gmail.com>
2017-06-24 00:24:10 +0200

    Remove useless imports

--- FourmilliereAL.Core/Fabriques/FabriqueAttitude.cs
+++ FourmilliereAL.Core/Fabriques/FabriqueAttitude.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FourmilliereAL.Core
+﻿namespace FourmilliereAL.Core
 {
     public class FabriqueAttitude
     {

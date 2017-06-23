e7e8bcf0ae3bb0cfa1a6b24f53d0188beb627b10
Sorion <maxime.pelte@gmail.com>
2017-06-23 23:58:39 +0200

    Define a new WindowViewModel for custom flat window, prepare the new FlatWindow

---
+++ FourmilliereAL/FlatWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace FourmilliereAL
+{
+    /// <summary>
+    /// Logique d'interaction pour Window1.xaml
+    /// </summary>
+    public partial class FlatWindow : Window
+    {
+        public FlatWindow()
+        {
+            InitializeComponent();
+            DataContext = new WindowViewModel(this);
+        }
+    }
+}
---
+++ FourmilliereAL/ViewModel/WindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using FourmilliereAL.Core;
+using System.Windows;
+
+namespace FourmilliereAL
+{
+    /// <summary>
+    /// Modèle de vue pour la fenêtre custom flat design
+    /// </summary>
+    class WindowViewModel : BaseViewModel
+    {
+        #region Membres privés
+        /// <summary>
+        /// La fenêtre que ce modèle de vue controle
+        /// </summary>
+        Window window;
+        #endregion
+
+        #region Constructeur
+        /// <summary>
+        /// Le constructor par défaut
+        /// </summary>
+        public WindowViewModel(Window window)
+        {
+            this.window = window;
+        }
+        #endregion
+    }
+}

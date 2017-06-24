384c8ed1a9f12f5703d7f654cd978d0a40b11bf3
Sorion <maxime.pelte@gmail.com>
2017-06-24 02:44:20 +0200

    Better mouse location tracking

--- FourmilliereAL/ViewModel/WindowViewModel.cs
+++ FourmilliereAL/ViewModel/WindowViewModel.cs
@@ -133,21 +133,11 @@ namespace FourmilliereAL
         #endregion
 
         #region Helper privé
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool GetCursorPos(ref Win32Point pt);
 
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct Win32Point
+        public Point GetMousePosition()
         {
-            public Int32 X;
-            public Int32 Y;
-        };
-        public static Point GetMousePosition()
-        {
-            Win32Point w32Mouse = new Win32Point();
-            GetCursorPos(ref w32Mouse);
-            return new Point(w32Mouse.X, w32Mouse.Y);
+            var position = Mouse.GetPosition(window);
+            return new Point(position.X + window.Left, position.Y + window.Top);
         }
         #endregion
     }

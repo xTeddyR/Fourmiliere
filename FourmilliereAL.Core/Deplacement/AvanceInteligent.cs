942dea35d1465a8e082ac9f1c605c6a51ff967fd
Julien Bertauld <julien.bertauld@gmail.com>
2017-06-26 23:39:43 +0200

    pythagoooree

---
+++ FourmilliereAL.Core/Deplacement/AvanceInteligent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourmilliereAL
+{
+    class AvanceInteligent : Deplacement
+    {
+        public override void Avance(Fourmi fourmi, int dimX, int dimY)
+        {
+            /*
+            if (FourmiY < objetY) { FourmiY++; };
+            if (FourmiY > objetY) { FourmiY--; };
+            if (FourmiX < objetX) { FourmiX++; };
+            if (FourmiX > objetX) { FourmiX--; };*/
+        }
+
+        private Double Distance(Location locationA, Location locationB)
+        {
+            int dx = Math.Abs(locationA.X - locationB.X);
+            int dy = Math.Abs(locationA.Y - locationB.Y);
+            return Math.Sqrt((dx * dx) + (dy * dy));
+        }
+    }
+}
